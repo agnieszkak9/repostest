4f2160161c90d92bae90de6729fae88918466563
agnieszkak9 <akuracinska99@gmail.com>
2017-09-07 08:40:54 +0200

    Sito eratonesa

--- Klasa_3/hello.cpp
+++ Klasa_3/hello.cpp
@@ -7,17 +7,40 @@
 using namespace std;
 #include <iostream>
 #define ROK_TERAZ 2017
+#define ROK_CPP 1983
+
+int parzyste (int n ){
+
+    int i;
+    for (i = 0; i <= n; i += 2)
+    cout << i << " ";
+    return i / 2;
+}
 
 int main(int argc, char **argv)
 {
     string imie;
-    int wiek;
+    int wiek, rok_urodzenia;
 	cout << "Cześć, jak masz na imię?" << endl;
     cin >> imie; 
     cout << "Witaj " << imie << endl;
     cout << "Ile masz lat?" << endl;
     cin >> wiek;
-    cout << "Urodziłeś/aś się w " << ROK_TERAZ - wiek << endl;
+    rok_urodzenia = ROK_TERAZ - wiek;
+    cout << "Urodziłeś/aś się w " << rok_urodzenia << endl;
+    if (ROK_CPP > rok_urodzenia){
+        cout << "Jesteś ode mnie starszy!" << endl;}
+    else if (ROK_CPP < rok_urodzenia){
+        cout << "Jesteś ode mnie młodszy!" << endl;}
+    else if (ROK_CPP == rok_urodzenia){
+        cout << "Jesteśmy w tym samym wieku!" << endl;}
+        
+    int n;
+    cout << "Podaj liczbę naturalną: ";
+    cin >> n;
+    parzyste(n);
+    cout << endl;
+    cout << "Ilość parzystych: ";
     
 	return 0;
 }
--- Klasa_3/hello.py
+++ Klasa_3/hello.py
@@ -6,11 +6,28 @@
 #  
 
 ROK_TERAZ = 2017
+ROK_PYTHON = 1991
+
+def parzyste(n):
+    ile = list(range(0, n+1, 2))
+    print (ile)
+    return len(ile)
 
 def main(args):
     imie = input("Jak masz na imię? ")
     print("Witaj", imie)
-
+    wiek = int(input("Ile masz lat? "))
+    rok_urodzenia = ROK_TERAZ - wiek
+    print("Urodziłeś/aś sie w " , rok_urodzenia)
+    if ROK_PYTHON > rok_urodzenia:
+        print("Jesteś ode mnie starszy!")
+    elif ROK_PYTHON < rok_urodzenia:
+        print ("Jesteś ode mnie młodszy!")
+    elif ROK_PYTHON == rok_urodzenia:
+        print("Jesteśmy w tym samym wieku!") 
+        
+    n = int(input("Podaj liczbe naturalną: "))
+    print('Ilość parzystych: ', parzyste(n))
     return 0
 
 if __name__ == '__main__':
