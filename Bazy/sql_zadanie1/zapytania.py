a565ad833438224e26aa222a6d6b006f39472c88
agnieszkak9 <akuracinska99@gmail.com>
2017-10-19 07:49:26 +0200

    kwerenda e

--- Bazy/sql_zadanie1/zapytania.py
+++ Bazy/sql_zadanie1/zapytania.py
@@ -7,7 +7,10 @@ import sqlite3
 
 def kw_c(cur):
     cur.execute ("""
-        SELECT SUM(placa) FROM pracownicy
+        SELECT siedziba, SUM(placa) AS pensje FROM dzial, pracownicy
+        WHERE dzial.id = pracownicy.id_dzial
+        GROUP BY siedziba 
+        ORDER BY pensje ASC
     """)
     
     wyniki = cur.fetchall()
@@ -28,11 +31,22 @@ def kw_d(cur):
     for rekord in wyniki:
         print(tuple(rekord))
 
+def kw_e(cur):
+    cur.execute ("""
+        SELECT nazwisko, stanowisko, placa * premia.premia FROM pracownicy, premia
+        WHERE  premia.id = pracownicy.stanowisko
+    """)
+    
+    wyniki = cur.fetchall()
+    for rekord in wyniki:
+        print(tuple(rekord))
+
 def main(args):
     con = sqlite3.connect('pracownicy.sqlite3')
     cur = con.cursor()  # utworzenie kursora
-    
-    kw_d(cur)
+    #kw_c(cur)
+    #kw_d(cur)
+    kw_e(cur)
     return 0
 
 if __name__ == '__main__':
