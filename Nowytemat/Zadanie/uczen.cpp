cce7bd54a26ef0a2a1c95e32ebad717a7970277e
kl3ag2 <you@example.com>
2018-01-23 08:41:56 +0100

    klasy

---
+++ Nowytemat/Zadanie/uczen.cpp
@@ -0,0 +1,27 @@
+/*
+ * uczen.cpp
+ * 
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+class Uczen {
+private:
+    string imie;     
+    string nazwisko; 
+public:
+    Uczen(string, string)
+    klasa();
+    
+};
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
